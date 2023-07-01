321c4d92b0b73e0fe69507ed73d96c7380e1b74f
Darshan Patil <iampatildarsh>
2023-07-01 11:58:12 +0530

    String index Programme

---
+++ com/practice/StringIndex.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class StringIndex {
+    public static void main(String[] args) {
+
+        String str = "darshan";
+        int i = str.indexOf('s');
+        System.out.println(i);
+    }
+}
